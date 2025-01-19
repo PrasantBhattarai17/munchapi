3c1d1482ce1a9c5197984fc3908b383401cba92d
notPrasantesp17 <punkersyt@gmail.com>
2025-01-19 12:43:22 +0545

    created a post request temporarily for the api

--- MunchyAPI/Controllers/MunchyAPIController.cs
+++ MunchyAPI/Controllers/MunchyAPIController.cs
@@ -10,17 +10,53 @@ namespace MunchyAPI.Controllers
     public class MunchyAPIController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<MunchDTO>> getMunch()
         {
             return Ok(MunchStore.MunchList);
 
         }
 
-        [HttpGet("{id:int} ")]
-        public ActionResult<MunchDTO> getMunchy(int id)
+        [HttpGet("{id:int}",Name="GetMunchy")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<MunchDTO> getmunchy(int id)
         {
-            return Ok(MunchStore.MunchList.FirstOrDefault(u=>u.Id==id));
+            if (id == 0)
+            {
+                return BadRequest();
+            }
 
+            var munch = Ok(MunchStore.MunchList.FirstOrDefault(u => u.Id == id));
+
+            if (munch == null)
+            {
+
+                return NotFound();
+            }
+
+            return Ok(munch);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<MunchDTO> CreateMunchy([FromBody] MunchDTO munchDTO)
+        {
+            if (munchDTO == null)
+            {
+                return BadRequest(munchDTO);
+            }
+            if (munchDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            munchDTO.Id = MunchStore.MunchList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            MunchStore.MunchList.Add(munchDTO);
+
+            return CreatedAtRoute("GetMunchy",new {id=munchDTO.Id},munchDTO);
         }
     }
 }
